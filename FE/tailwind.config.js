5b32e80191ed8724c42e2230509d0980c02fc47d
이민수 <kipperhr@naver.com>
2024-04-23 09:57:17 +0900

    [FE][Design] 메인 컬러 설정
    Tailwind CSS Intellisense 플러그인을 vsCode에서 받으면 색상을 미리 확인할 수 있습니다. cusColor1 - 5로 해놨습니다!

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -9,7 +9,14 @@ export default {
       fontFamily: {
         cusFont1: ['비트비트체v2'], // 비트비트체v2
         cusFont2: ['Orbit'], // Orbit
-      }
+      },
+      colors: {
+        'cusColor1': '#186F65', // 진초록색
+        'cusColor2': '#795458', // 짙은 갈색
+        'cusColor3': '#FFC94A', // 진노란색
+        'cusColor4': '#FFFAB7', // 연노란색
+        'cusColor5': '#5BBCFF', // 하늘색
+      },
     },
   },
   plugins: [],
