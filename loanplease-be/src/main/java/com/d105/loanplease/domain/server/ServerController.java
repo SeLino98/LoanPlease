9382d6d22dc0fb6c890ce8a20d1de25a284f141d
조남현 <zetta_byte@naver.com>
2024-04-25 17:43:07 +0900

    api TEST

--- loanplease-be/src/main/java/com/d105/loanplease/domain/server/ServerController.java
+++ loanplease-be/src/main/java/com/d105/loanplease/domain/server/ServerController.java
@@ -1,11 +1,13 @@
 package com.d105.loanplease.domain.server;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@CrossOrigin("*")
 public class ServerController {
 
     @GetMapping("/hi")
