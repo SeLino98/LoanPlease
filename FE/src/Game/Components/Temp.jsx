722ff41e6a4ae3063506f7f630cd23a0e6ada47c
이민수 <kipperhr@naver.com>
2024-04-25 15:49:11 +0900

    [FE][Fix] API 주소 임시 변경

--- FE/src/Game/Components/Temp.jsx
+++ FE/src/Game/Components/Temp.jsx
@@ -7,7 +7,7 @@ function Temp() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://loanpleaseBE:8080/apitest");
+        const response = await axios.get("http://loanpleasebe:8080/apitest");
         setData(response.data);
       } catch (error) {
         console.error('API 요청 중 오류 발생:', error);
