ec7821609a83843fe380172ef1c3717706e8e6f5
이중원 <wnddnjs823@ajou.ac.kr>
2024-05-02 10:13:42 +0900

    [BE][Feat] LoanServiceTest 파일 추가

---
+++ loanplease-be/src/test/java/com/d105/loanplease/loan/LoanServiceTest.java
@@ -0,0 +1,13 @@
+package com.d105.loanplease.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LoanServiceTest {
+
+    @Test
+    void 대출상품구매() {
+
+    }
+}
