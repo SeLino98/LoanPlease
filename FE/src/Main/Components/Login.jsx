5176e2f8df7b2e86470cc72ec9d63dad0b323a0c
김민우 <kmw7532@naver.com>
2024-05-09 13:10:25 +0900

    [FE][feat] #S10P31D105-315 USER API 연동하기 - 회원가입 (구현 중)

--- FE/src/Main/Components/Login.jsx
+++ FE/src/Main/Components/Login.jsx
@@ -132,8 +132,8 @@ function Login() {
             if (mydata.nick != "-") {
               setIsLogin(true);
             } else {
-              let url = "http://localhost:8080/oauth2/authorization/google";
-              // let url = "https://loanplease.kr/oauth2/authorization/google";
+              // let url = "http://localhost:8080/oauth2/authorization/google";
+              let url = "https://loanplease.kr/oauth2/authorization/google";
               location.href = url;
             }
           }}
--- FE/vite.config.js
+++ FE/vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080/api",
+        target: "https://loanplease.kr/api",
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
