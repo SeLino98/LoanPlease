d36002c71d59adaeedbc264a3b622dd6181f2f8f
김인호 <linos98@naver.com>
2024-05-14 12:34:24 +0900

    [BE][Swagger] 스웨거 https 통신 허용 및 config 파일, JWT 추가

---
+++ loanplease-be/src/main/java/com/d105/loanplease/global/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.d105.loanplease.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@OpenAPIDefinition(
+        info=@Info(title = "LoanPlease",
+                description = "대출의 프로세스와 이해를 게임을 통해 경험하는 프로젝트 \n " +
+                        "   싸피 10기 자율 프로젝트 D105팀 ",
+                version ="v6"))
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("Bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+        Server server = new Server();
+        server.setUrl("https://loanplease.kr"); // https://에 접근 가능하게 설정
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement))
+                .servers(List.of(server));
+    }
+
+}
