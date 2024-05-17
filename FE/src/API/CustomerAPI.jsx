4d3edb6951ba7081494aa82838eced8a1a5cbdb6
이민수 <kipperhr@naver.com>
2024-05-17 16:17:11 +0900

    임시9

--- FE/src/API/CustomerAPI.jsx
+++ FE/src/API/CustomerAPI.jsx
@@ -7,7 +7,7 @@ const cookies = new Cookies();
 async function fetchCustomerRequest() {
   const token = cookies.get('Authorization');
   try {
-    const response = await fetch('https://loanplease.kr/game/loanrequest', {
+    const response = await fetch('https://loanplease.kr/api/game/loanrequest', {
       headers: {
         'Authorization': `Bearer ${token}`
       }
@@ -19,7 +19,7 @@ async function fetchCustomerRequest() {
     }
     return await response.json();
   } catch (error) {
-    console.error("Failed to fetch data:", error);
+    console.error(error);
     throw error;  // 에러를 던지거나 에러 상태를 반환할 수 있습니다.
   }
 }
@@ -47,7 +47,7 @@ async function postScoreRequest(num, data) {
   const token = cookies.get('Authorization');
 
   try {
-    const response = await fetch(`https://loanplease.kr/game/score/${num}`, {
+    const response = await fetch(`https://loanplease.kr/api/game/score/${num}`, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${token}`
@@ -68,7 +68,7 @@ async function fetchGetawayRequest(data) {
   const token = cookies.get('Authorization');
 
   try {
-    const response = await fetch(`https://loanplease.kr/game/getaway`, {
+    const response = await fetch(`https://loanplease.kr/api/game/getaway`, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${token}`
@@ -89,7 +89,7 @@ async function patchScoreRequest(score) {
   const token = cookies.get('Authorization');
 
   try {
-    const response = await fetch(`https://loanplease.kr/game/score?score=${score}`, {
+    const response = await fetch(`https://loanplease.kr/api/game/score?score=${score}`, {
       method: 'PATCH',
       headers: {
         'Authorization': `Bearer ${token}`
@@ -110,7 +110,7 @@ async function postUseItem(num) {
   const token = cookies.get('Authorization');
 
   try {
-    const response = await fetch(`https://loanplease.kr/game/use-item`, {
+    const response = await fetch(`https://loanplease.kr/api/game/use-item`, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${token}`
