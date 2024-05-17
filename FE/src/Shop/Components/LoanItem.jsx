768a31277f03dfe249828f35365fcef2b0902f89
BaekJaehee <151-51@naver.com>
2024-05-17 17:41:12 +0900

    배경수정2

--- FE/src/Shop/Components/LoanItem.jsx
+++ FE/src/Shop/Components/LoanItem.jsx
@@ -78,7 +78,7 @@ function LoanItem({ openLoanItemModal, openWarningModal, loanItems, point, produ
             className={`relative flex-grow-1 w-[300px] h-[85%] border-2 ${
               purchasedProducts.includes(item.loanId)
                 ? "bg-stone-300 cursor-not-allowed text-gray-600"
-                : item.color}
+                : `${item.color}`}
             } px-3 py-4 rounded-lg border-black`}
           >
             <p className="font-cusFont1 text-2xl py-4 my-2 h-[20%]">{item.name}</p>
