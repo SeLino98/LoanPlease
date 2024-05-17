367d999d9a8150bfad9930eff5df07e060600ddd
이민수 <kipperhr@naver.com>
2024-05-17 14:54:45 +0900

    원상복구

--- FE/src/API/CustomerAPI.jsx
+++ FE/src/API/CustomerAPI.jsx
@@ -7,7 +7,7 @@ const cookies = new Cookies();
 async function fetchCustomerRequest() {
   const token = cookies.get('Authorization');
   try {
-    const response = await fetch('https://loanplease.kr/game/loanrequest', {
+    const response = await fetch(import.meta.env.VITE_BASE_URL + 'game/loanrequest', {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`
@@ -26,7 +26,7 @@ async function fetchCustomerRequest() {
 async function fetchUserRequest() {
   const token = cookies.get('Authorization');
   try {
-    const response = await fetch('https://loanplease.kr/api/user-info', {
+    const response = await fetch(import.meta.env.VITE_BASE_URL + 'api/user-info', {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`
@@ -69,7 +69,7 @@ async function fetchGetawayRequest(data) {
   const token = cookies.get('Authorization');
 
   try {
-    const response = await fetch('https://loanplease.kr/game/getaway', {
+    const response = await fetch(import.meta.env.VITE_BASE_URL + `game/getaway`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -78,7 +78,6 @@ async function fetchGetawayRequest(data) {
       body: JSON.stringify(data)
     });
     if (!response.ok) {
-      console.error("Failed to fetch data:", error);
       throw new Error('Network response was not ok');
     }
     return await response.json();
