8a74a24092c0019fc013439c3ea5c12567829267
이중원 <wnddnjs823@ajou.ac.kr>
2024-05-17 16:04:53 +0900

    [BE][Update] Store, Game Controller의 기본 url에 api 추가

--- loanplease-be/src/main/java/com/d105/loanplease/domain/game/controller/GameController.java
+++ loanplease-be/src/main/java/com/d105/loanplease/domain/game/controller/GameController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/game")
+@RequestMapping("api/game")
 public class GameController {
 
     private final GameService gameService;
--- loanplease-be/src/main/java/com/d105/loanplease/domain/store/adapter/in/StoreController.java
+++ loanplease-be/src/main/java/com/d105/loanplease/domain/store/adapter/in/StoreController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/store")
+@RequestMapping("api/store")
 @Slf4j
 public class StoreController {
 
