aeda03f65c297d08e8b516e547bfb1514f630556
김인호 <linos98@naver.com>
2024-05-17 14:06:51 +0900

    [BE][CORS] AuthController_CORS수정

--- loanplease-be/src/main/java/com/d105/loanplease/LoanpleaseApplication.java
+++ loanplease-be/src/main/java/com/d105/loanplease/LoanpleaseApplication.java
@@ -16,43 +16,40 @@ public class LoanpleaseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LoanpleaseApplication.class, args);
 	}
-
 	@Bean
 	public CorsFilter corsFilter() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
-		config.addAllowedOriginPattern("http://loanplease.kr");
+		config.addAllowedOriginPattern("http://loanplease.kr"); // 허용할 도메인을 명시
 		config.addAllowedOriginPattern("https://loanplease.kr");
-		config.addAllowedOriginPattern("http://localhost:8080");
+		config.addAllowedOriginPattern("http://localhost:8080");// 추가로 허용할 도메인
 		config.addAllowedHeader("*");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("PATCH");
-		config.addAllowedMethod("DELETE");
-		config.addAllowedMethod("OPTIONS"); // 추가: OPTIONS 메서드 허용
+		config.addAllowedMethod("*");
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
 	}
-
-
 //	@Bean
 //	public CorsFilter corsFilter() {
 //		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 //		CorsConfiguration config = new CorsConfiguration();
 //		config.setAllowCredentials(true);
-//		config.addAllowedOriginPattern("http://loanplease.kr"); // 허용할 도메인을 명시
+//		config.addAllowedOriginPattern("http://loanplease.kr");
 //		config.addAllowedOriginPattern("https://loanplease.kr");
-//		config.addAllowedOriginPattern("http://localhost:8080");// 추가로 허용할 도메인
+//		config.addAllowedOriginPattern("http://localhost:8080");
 //		config.addAllowedHeader("*");
 //		config.addAllowedMethod("GET");
 //		config.addAllowedMethod("POST");
 //		config.addAllowedMethod("PUT");
+//		config.addAllowedMethod("PATCH");
 //		config.addAllowedMethod("DELETE");
+//		config.addAllowedMethod("OPTIONS"); // 추가: OPTIONS 메서드 허용
 //		source.registerCorsConfiguration("/**", config);
 //		return new CorsFilter(source);
 //	}
+
+
+
 //	@Bean
 //	public WebMvcConfigurer corsConfigurer() {
 //		return new WebMvcConfigurer() {
