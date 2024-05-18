f093d891587db3a8b62a9588af29b877a81a0b1a
BaekJaehee <151-51@naver.com>
2024-05-18 18:03:06 +0900

    useRef import 추가

--- FE/src/Shop/Components/Shop.jsx
+++ FE/src/Shop/Components/Shop.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import useStore from "../../Store/ShopStore";
 import { MainStore } from "../../Main/Store";
