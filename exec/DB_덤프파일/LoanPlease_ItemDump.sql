7b640d83f73eb42b9c73558d8bcb90752acc3b3d
이중원 <wnddnjs823@ajou.ac.kr>
2024-05-20 09:46:49 +0900

    [ALL][Feat] 포팅메뉴얼 제출

---
+++ exec/DB_덤프파일/LoanPlease_ItemDump.sql
@@ -0,0 +1,6 @@
+insert into item_tb(price, item_id, content, name, img)
+values(500,1,'고객을 VIP 룸으로 보내 조건에 상관 없이 최고점을 획득합니다.','VIP', 'vip');
+insert into item_tb(price, item_id, content, name, img)
+values(1000,2,'게임 시간을 1분 추가합니다.','시간 연장','timeExtension');
+insert into item_tb(price, item_id, content, name, img)
+values(500,3,'보호막을 통해 점수 감점을 보호해줍니다.','보호막','shield');
